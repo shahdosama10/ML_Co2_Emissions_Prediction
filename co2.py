2b7097344d0921f3146bb91309117f96ff3d8a83
Ahmed Saad <102562587+ahmedsaad123456@users.noreply.github.com>
2024-11-23 20:27:48 +0200

    clean the code

--- co2.py
+++ co2.py
@@ -1,4 +1,5 @@
 # %%
+# =================================================================================================
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -12,25 +13,21 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
-
-
-
-# %% [markdown]
-# ## a) Load the "co2_emissions_data.csv" dataset.
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# a) Load the "co2_emissions_data.csv" dataset.
+# =================================================================================================
+
 df = pd.read_csv('co2_emissions_data.csv')
 
-
-# %% [markdown]
-# 
-# ## b) Perform analysis on the dataset to
-
-# %% [markdown]
-# 
-# >#### i) check whether there are missing values
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# b) Perform analysis on the dataset to
+# =================================================================================================
+
+# i) check whether there are missing values
 
 # to know the number of the rows
 print(len(df))
@@ -40,30 +37,31 @@ df.info()
 
 # according to the output there aren't any missing values
 
-
-# %% [markdown]
-# >#### ii) check whether numeric features have the same scale
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# ii) check whether numeric features have the same scale
+# =================================================================================================
+
 # To get min and max values for each column
 
 df.describe().T
 
 # according to the output the numeric features dosn't have the same scale
 
-
-# %% [markdown]
-# >#### iii) visualize a pairplot in which diagonal subplots are histograms
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# iii) visualize a pairplot in which diagonal subplots are histograms
+# =================================================================================================
+
 sns.pairplot(df, hue='Emission Class' , diag_kind='hist')
 
-
-# %% [markdown]
-# >#### iv) visualize a correlation heatmap between numeric columns
-# 
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# iv) visualize a correlation heatmap between numeric columns
+# =================================================================================================
 
 # select only the numeric columns
 
@@ -75,19 +73,21 @@ corr = df_num.corr()
 sns.heatmap(corr, annot=True)
 
 
-
-# %% [markdown]
-# ## c) Preprocess the data such that:
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# c) Preprocess the data such that:
+# =================================================================================================
 # get copy from the original to preprocess
 
 df_pre = df.copy()
 
-# %% [markdown]
-# >#### i) the features and targets are separated
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# i) the features and targets are separated
+# =================================================================================================
+
 targets_columns = ['CO2 Emissions(g/km)' , 'Emission Class']
 
 df_targets = df_pre[targets_columns]
@@ -95,11 +95,11 @@ df_features = df_pre.drop(columns=targets_columns)
 
 print(df_features.head())
 print(df_targets.head())
-
-# %% [markdown]
-# >#### ii) categorical features and targets are encoded
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# ii) categorical features and targets are encoded
+# =================================================================================================
 
 # set the categorical features
 
@@ -113,9 +113,9 @@ df_features[label_col_encoded_features] = df_features[label_col_encoded_features
 df_features.head()
 
 
-
-
+# =================================================================================================
 # %%
+# =================================================================================================
 
 # get the unique values for targets to show the order of the values
 df_targets['Emission Class'].unique()
@@ -139,13 +139,11 @@ df_targets.loc[:, ordinal_col_encoded_targets] = oe.fit_transform(df_targets[ord
 
 df_targets.head()
 
-# %% [markdown]
-# 
-# >>##### Based on the correlation heatmap, select two features to be the independent variables of your model.
-# 
-
+# =================================================================================================
 # %%
-
+# =================================================================================================
+# Based on the correlation heatmap, select two features to be the independent variables of your model.
+# =================================================================================================
 
 df_train_combined = pd.concat([df_features, df_targets], axis=1)
 corr_matrix = df_train_combined.corr()
@@ -153,12 +151,11 @@ plt.figure(figsize=(12, 10))
 sns.heatmap(corr_matrix, annot=True , cmap='coolwarm')
 
 # Fuel Consumption Comb (L/100 km) and Cylinders
-
-# %% [markdown]
-# >#### iii) the data is shuffled and split into training and testing set
-# 
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# iii) the data is shuffled and split into training and testing set
+# =================================================================================================
 
 # make the 80% from the data training set and 20% from the data testing set
 # random state to ensure that the split return the same data each run
@@ -169,13 +166,11 @@ print(len(df_features_train))
 print(len(df_features_test))
 print(len(df_targets_train))
 print(len(df_targets_test))
-
-# %% [markdown]
-# >#### iv) numeric features are scaled
-# 
-
+# =================================================================================================
 # %%
-
+# =================================================================================================
+# iv) numeric features are scaled
+# =================================================================================================
 
 scaler = RobustScaler()
 
@@ -197,8 +192,10 @@ print(df_features_test.describe().T)
 
 
 
-
+# =================================================================================================
 # %%
+# =================================================================================================
+
 selected_features = ['Fuel Consumption Comb (L/100 km)' , 'Cylinders']
 
 df_selected_features_train = df_features_train[selected_features]
@@ -207,22 +204,21 @@ df_selected_features_test = df_features_test[selected_features]
 
 print(df_selected_features_train.head())
 print(df_selected_features_test.head())
-
-# %% [markdown]
-# ## **d) Implement linear regression using gradient descent from scratch**
-
-# %% [markdown]
-# >#### ***Hypothesis Function***
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# d) Implement linear regression using gradient descent from scratch
+# =================================================================================================
+
+# Hypothesis Function
 def hypothesis(x, theta):
     y_predections = np.dot(x, theta)
     return y_predections
-
-# %% [markdown]
-# >#### ***Cost Function***
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# Cost Function
+# =================================================================================================
 def cost_fun(x, y, theta):
     m = len(y)
     err = 0
@@ -232,11 +228,11 @@ def cost_fun(x, y, theta):
     cost = (1 / (2 * m)) * err
     return cost
 
-
-# %% [markdown]
-# >#### ***Gradient Descent Implementation***
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# Gradient Descent Implementation
+# =================================================================================================
 def gradient_descent(x, y ,theta, alpha, iterations):
     m = len(y)
     costs = []
@@ -254,11 +250,12 @@ def gradient_descent(x, y ,theta, alpha, iterations):
         costs.append(cost_fun(x, y, theta))
     
     return theta, costs
-
-# %% [markdown]
-# >#### ***Gradient Descent Run***
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# Gradient Descent Run
+# =================================================================================================
+
 n = 2 # number of features
 theta = np.zeros(n+1) # added 1 for theta0
 alpha = 0.03
@@ -272,11 +269,11 @@ print("our theta", theta)
 for i in range(0, 500, 50): 
     print(costs[i])
 
-
-# %% [markdown]
-# >#### ***R<sup>2</sup> Score***
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# R2 Score
+# =================================================================================================
 x_test = df_selected_features_test.to_numpy()
 x_test = np.c_[np.ones((x_test.shape[0], 1)), x_test]
 y_test = pd.DataFrame(df_targets_test, columns=df_targets.columns).drop(columns=['Emission Class']).to_numpy().reshape(-1, 1)
@@ -284,25 +281,22 @@ y_predictions = np.array([hypothesis(row, theta) for row in x_test]).reshape(-1,
 
 r2 = r2_score(y_test, y_predictions)  # R2 Score
 print("R^2 Score:", r2)
-
-# %% [markdown]
-# >#### ***Gradient Descent Cost Function Plot***
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# Gradient Descent Cost Function Plot
+# =================================================================================================
 plt.plot(range(iterations), costs)
 plt.title("Gradient Descent Cost Function Plot")
 plt.xlabel("Iterations")
 plt.ylabel("Cost")
 plt.show()
-
-# %% [markdown]
-# ## **e) Fit a logistic regression model to the data**
-
-# %% [markdown]
-# >#### ***Prepare the Data***
-# 
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# e) Fit a logistic regression model to the data
+# =================================================================================================
+# Prepare the Data
 x_train = df_selected_features_train
 x_test = df_selected_features_test
 
@@ -311,12 +305,11 @@ y_test = df_targets_test.drop(columns=["CO2 Emissions(g/km)"])
 
 y_train = y_train.astype(int)
 y_test = y_test.astype(int)
-
-
-# %% [markdown]
-# >#### ***Initialize and Train the Model***
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# Initialize and Train the Model
+# =================================================================================================
 model = SGDClassifier(loss='log_loss', max_iter=1000, random_state=42)
 
 # convert the y_train and y_test from 2d arrays to 1d arrays
@@ -324,15 +317,15 @@ y_train = y_train.to_numpy().ravel()
 y_test = y_test.to_numpy().ravel()
 
 model.fit(x_train, y_train)
-
-# %% [markdown]
-# >#### ***Make Predictions and Evaluate the Model***
-
+# =================================================================================================
 # %%
+# =================================================================================================
+# Make Predictions and Evaluate the Model
+# =================================================================================================
 y_pred = model.predict(x_test)
 
 accuracy = accuracy_score(y_test, y_pred)
 
 print(f'Accuracy: {accuracy * 100:.2f}%')
 
-
+# =================================================================================================
